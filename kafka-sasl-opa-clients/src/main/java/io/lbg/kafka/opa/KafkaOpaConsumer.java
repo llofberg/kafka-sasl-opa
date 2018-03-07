874f9b84c2b2c800e652a03d9276f9063830ca9f
Lenny Lofberg <lenny@Lennys-MacBook-Pro-6387.local>
2018-03-06 22:43:29 -0800

    WIP: Java clients

---
+++ kafka-sasl-opa-clients/src/main/java/io/lbg/kafka/opa/KafkaOpaConsumer.java
@@ -0,0 +1,107 @@
+package io.lbg.kafka.opa;
+
+import com.google.gson.Gson;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
+@Slf4j
+public class KafkaOpaConsumer {
+
+  private final static String TOPIC = "X";
+  private final static String BOOTSTRAP_SERVERS = "broker:9093";
+  private boolean keepRunning = true;
+
+  public static void main(String[] args) {
+    new KafkaOpaConsumer().run();
+  }
+
+  private void run() {
+
+    final Properties props = new Properties();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "ssl-sasl-host");
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+    props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+
+    props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/etc/kafka/secrets/kafka.consumer.truststore.jks");
+    props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "confluent");
+    props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/etc/kafka/secrets/kafka.consumer.keystore.jks");
+    props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "confluent");
+    props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "confluent");
+
+    props.put(SaslConfigs.SASL_MECHANISM, "GSSAPI");
+    props.put(SaslConfigs.SASL_KERBEROS_SERVICE_NAME, "kafka");
+
+    // Create the consumer using props.
+    final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+
+    // Subscribe to the topic.
+    consumer.subscribe(Collections.singletonList(TOPIC));
+
+    try {
+      while (keepRunning) {
+        final ConsumerRecords<Long, String> consumerRecords =
+          consumer.poll(10000);
+
+        consumerRecords.forEach(record -> {
+          log.error("{} => {}", record, allow(record.value()));
+        });
+
+        consumer.commitAsync();
+      }
+    } finally {
+      consumer.close();
+      System.out.println("DONE");
+    }
+  }
+
+  private Gson gson = new Gson();
+
+  private boolean allow(String input) {
+    try {
+      HttpURLConnection conn = (HttpURLConnection)
+        new URL("http://opa:8181/v1/data/kafka/message/allow").openConnection();
+
+      conn.setDoOutput(true);
+      conn.setRequestMethod("POST");
+      conn.setRequestProperty("Content-Type", "application/json");
+
+      OutputStream os = conn.getOutputStream();
+      os.write(("{\"input\":" + input + "}").getBytes());
+      os.flush();
+
+      if (log.isTraceEnabled()) {
+        log.trace("Response code: {}, Request data: {}", conn.getResponseCode(), input);
+      }
+
+      @Cleanup BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+      String json = br.readLine();
+      log.error("REQ: {} RSP: {}", input, json);
+      Map map = gson.fromJson(json, Map.class);
+      return map.containsKey("result") && (boolean) map.get("result");
+    } catch (IOException e) {
+      return false;
+    }
+  }
+}
